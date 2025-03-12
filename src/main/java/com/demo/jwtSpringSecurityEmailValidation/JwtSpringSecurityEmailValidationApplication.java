0c6688aa2776694fbb5274df27f07e2bd542ddd5
raj <rajmandal147@gmail.com>
2025-03-12 20:34:24 +0530

    first

---
+++ src/main/java/com/demo/jwtSpringSecurityEmailValidation/JwtSpringSecurityEmailValidationApplication.java
@@ -0,0 +1,13 @@
+package com.demo.jwtSpringSecurityEmailValidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSpringSecurityEmailValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSpringSecurityEmailValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/jwtSpringSecurityEmailValidation/JwtSpringSecurityEmailValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.jwtSpringSecurityEmailValidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSpringSecurityEmailValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
